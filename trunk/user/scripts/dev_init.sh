a435f672770a46ae0e1ed0b1fa05cc72e655d550
h8820 <101133120+h8820@users.noreply.github.com>
2025-03-12 16:03:13 +0800

    Update dev_init.sh

--- trunk/user/scripts/dev_init.sh
+++ trunk/user/scripts/dev_init.sh
@@ -6,13 +6,14 @@ mount -t sysfs sysfs /sys
 
 size_tmp="24M"
 size_var="4M"
+size_etc="6M"
 if [ "$1" == "-l" ]; then
 	size_tmp="8M"
 	size_var="1M"
 fi
 
 mount -t tmpfs tmpfs /dev   -o size=8K
-mount -t tmpfs tmpfs /etc   -o size=2M,noatime
+mount -t tmpfs tmpfs /etc   -o size=6M,noatime
 mount -t tmpfs tmpfs /home  -o size=1M
 mount -t tmpfs tmpfs /media -o size=8K
 mount -t tmpfs tmpfs /mnt   -o size=8K
