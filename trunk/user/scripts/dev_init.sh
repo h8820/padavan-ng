dc026cb1182b281304759a7dcea17230e1a10035
h8820 <101133120+h8820@users.noreply.github.com>
2025-03-12 18:16:33 +0800

    Update dev_init.sh

--- trunk/user/scripts/dev_init.sh
+++ trunk/user/scripts/dev_init.sh
@@ -4,15 +4,16 @@ mount -t proc proc /proc
 mount -t sysfs sysfs /sys
 [ -d /proc/bus/usb ] && mount -t usbfs usbfs /proc/bus/usb
 
-size_tmp="24M"
+size_tmp="20M"
 size_var="4M"
+size_etc="6M"
 if [ "$1" == "-l" ] ; then
 	size_tmp="8M"
 	size_var="1M"
 fi
 
 mount -t tmpfs tmpfs /dev   -o size=8K
-mount -t tmpfs tmpfs /etc   -o size=2M,noatime
+mount -t tmpfs tmpfs /etc   -o size=$size_etc,noatime
 mount -t tmpfs tmpfs /home  -o size=1M
 mount -t tmpfs tmpfs /media -o size=8K
 mount -t tmpfs tmpfs /mnt   -o size=8K
